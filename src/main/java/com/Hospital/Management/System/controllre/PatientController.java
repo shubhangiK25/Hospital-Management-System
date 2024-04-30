368000ec33be93e209fec880a775b84d7103c19a
shubhangik25 <khalkarshubhangi1595@gmail.com>
2024-04-30 10:31:51 -0700

    pushed create and get api

---
+++ src/main/java/com/Hospital/Management/System/controllre/PatientController.java
@@ -0,0 +1,45 @@
+package com.Hospital.Management.System.controllre;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Hospital.Management.System.entity.Patient;
+import com.Hospital.Management.System.repository.PatientRepository;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class PatientController {
+
+	
+	private PatientRepository patientRepository;
+
+	public PatientController(PatientRepository patientRepository) {
+		super();
+		this.patientRepository = patientRepository;
+	}
+	@PostMapping("/insert")
+	 public Patient createPatient(@RequestBody Patient patient)
+	 {
+		 return patientRepository.save(patient);
+	 }
+	@GetMapping("/get")
+	 public List<Patient> getPatient()
+	 {
+		 return patientRepository.findAll();
+	 }
+	
+	@GetMapping("/get/{userid}")
+	public Optional<Patient> getPatientById(@PathVariable Long userid)
+	{
+		return patientRepository.findById(userid);
+		
+	}
+}
